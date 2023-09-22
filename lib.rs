817e846ce9d25b91390b631da335afd872ce9974
Igor Berlenko <sevenflash@yandex.ru>
2023-09-22 18:07:52 +0800

    seventh

--- lib.rs
+++ lib.rs
@@ -1,14 +1,13 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
-use ink_lang as ink;
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 #[ink::contract]
 mod galaxy_contract {
-    use ink_prelude::vec::Vec;
-    use ink_storage::{traits::SpreadAllocate, Mapping};
+    use ink::prelude::vec::Vec;
+    use ink::storage::Mapping;
+    use ink::prelude::string::String;
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
-    #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         LayerAlreadyExists,
         LayerNotFound,
@@ -25,7 +24,6 @@ mod galaxy_contract {
     }
 
     #[ink(storage)]
-    #[derive(SpreadAllocate)]
     pub struct GalaxyContract {
         user_layers: Mapping<AccountId, Vec<String>>,
         layer_links: Mapping<(AccountId, String), String>,
@@ -43,8 +41,7 @@ mod galaxy_contract {
         #[ink(message)]
         pub fn create_layer(&mut self, layer_name: String, ipfs_link: String) -> Result<()> {
             let user = self.env().caller();
-            
-            let layers = self.user_layers.get(&user).unwrap_or(Vec::new()).to_vec();
+            let layers = self.user_layers.get(&user).unwrap_or_default().clone();
 
             if layers.contains(&layer_name) {
                 return Err(Error::LayerAlreadyExists);
@@ -64,7 +61,7 @@ mod galaxy_contract {
         pub fn resolve_link(&self, user: AccountId, layer_name: String) -> Result<String> {
             if let Some(layers) = self.user_layers.get(&user) {
                 if layers.contains(&layer_name) {
-                    if let Some(link) = self.layer_links.get(&(user, layer_name)) {
+                    if let Some(link) = self.layer_links.get(&(user, layer_name.clone())) {
                         return Ok(link.clone());
                     }
                 }
@@ -77,12 +74,11 @@ mod galaxy_contract {
     mod tests {
         use super::*;
         use ink_lang as ink;
-        use ink_env::{test, DefaultEnvironment};
 
         #[ink::test]
         fn test_create_layer() {
             let mut contract = GalaxyContract::new();
-            
+
             assert_eq!(contract.create_layer("Layer1".to_string(), "ipfs://link1".to_string()), Ok(()));
             assert_eq!(contract.create_layer("Layer1".to_string(), "ipfs://link2".to_string()), Err(Error::LayerAlreadyExists));
         }
@@ -90,22 +86,13 @@ mod galaxy_contract {
         #[ink::test]
         fn test_resolve_link() {
             let mut contract = GalaxyContract::new();
-
             let caller = AccountId::from([0x1; 32]);
-            test::set_caller::<DefaultEnvironment>(caller);
 
             assert_eq!(contract.resolve_link(caller, "Layer1".to_string()), Err(Error::LayerNotFound));
 
-            let creation_result = contract.create_layer("Layer1".to_string(), "ipfs://link1".to_string());
-            assert_eq!(creation_result, Ok(()));
+            contract.create_layer("Layer1".to_string(), "ipfs://link1".to_string()).unwrap();
 
-            let user_layers = contract.user_layers.get(&caller);
-            ink_env::debug_println!("User layers: {:?}", user_layers);
-
-            let link = contract.layer_links.get(&(caller.clone(), "Layer1".to_string()));
-            ink_env::debug_println!("Layer1 link: {:?}", link);
-
-            let resolve_result = contract.resolve_link(caller.clone(), "Layer1".to_string());
+            let resolve_result = contract.resolve_link(caller, "Layer1".to_string());
             assert_eq!(resolve_result, Ok("ipfs://link1".to_string()));
         }
     }
